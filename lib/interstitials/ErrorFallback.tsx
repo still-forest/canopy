3e692cdd19d9ccf2277226c4374d77add0543e68
John Szymanowski <github@john.szymanow.ski>
2025-05-13 20:59:56 +0800

    use code

--- lib/interstitials/ErrorFallback.tsx
+++ lib/interstitials/ErrorFallback.tsx
@@ -2,7 +2,7 @@ import { RotateCcw } from "lucide-react";
 
 import { Button } from "@/forms";
 import { Box, Flex } from "@/layout";
-import { Text } from "@/typography";
+import { Code } from "@/typography";
 
 import { ErrorOverlay } from "./ErrorOverlay";
 
@@ -19,9 +19,9 @@ export const ErrorFallback = ({ error }: ErrorFallbackProps) => {
     <ErrorOverlay message="Oops! Something went wrong.">
       <Flex direction="col" align="center" className="mt-8">
         <Box variant="muted" width="full" className="mb-8 p-4">
-          <Text variant="inherit" family="mono" align="center" size="xs">
+          <Code variant="inherit" align="center" size="xs">
             {error.message}
-          </Text>
+          </Code>
         </Box>
         <Button variant="primary" icon={<RotateCcw />} onClick={handleRetry}>
           Reload
--- lib/interstitials/NotFoundErrorPage.tsx
+++ lib/interstitials/NotFoundErrorPage.tsx
@@ -2,7 +2,7 @@ import { FileQuestion, Home } from "lucide-react";
 
 import { Button } from "@/forms";
 import { Box, Flex } from "@/layout";
-import { Text } from "@/typography";
+import { Code } from "@/typography";
 
 import { Interstitial } from "./Interstitial";
 
@@ -15,9 +15,9 @@ export const NotFoundErrorPage = ({ goHome }: NotFoundErrorPageProps) => {
     <Interstitial variant="warning" iconComponent={FileQuestion} message="404: Synergy Not Found.">
       <Flex direction="col" align="center" className="mt-8">
         <Box variant="muted" className="mb-8 w-full p-4">
-          <Text variant="inherit" family="mono" align="center" size="xs">
+          <Code variant="inherit" align="center" size="xs">
             Please realign strategic vision, optimize bandwidth, and ping stakeholders for a paradigm shift.
-          </Text>
+          </Code>
         </Box>
         {goHome && (
           <Button variant="primary" icon={<Home />} onClick={goHome}>
