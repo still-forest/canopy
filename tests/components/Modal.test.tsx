f72792aee52a5c039e40e5e7261b8054f37588ab
John Szymanowski <github@john.szymanow.ski>
2025-05-14 22:09:55 +0800

    tests

---
+++ tests/components/Modal.test.tsx
@@ -0,0 +1,90 @@
+import { render, screen, within } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+
+import { Modal } from "@/components";
+import { Button } from "@/forms";
+
+describe("Modal", () => {
+  test("renders a Modal", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Modal trigger={<Button>Open modal</Button>} title="Modal title" description="Modal description">
+        <p>Modal content</p>
+      </Modal>,
+    );
+
+    const trigger = screen.getByRole("button");
+    expect(trigger).toHaveTextContent("Open modal");
+
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+
+    await user.click(trigger);
+
+    const dialog = screen.getByRole("dialog");
+    expect(dialog.className).toBe(
+      "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
+    );
+
+    const title = within(dialog).getByText("Modal title");
+    expect(title.className).toBe("text-lg leading-none font-semibold");
+
+    const description = within(dialog).getByText("Modal description");
+    expect(description.className).toBe("text-muted-foreground text-sm");
+
+    const content = within(dialog).getByText("Modal content");
+    expect(content.className).toBe("");
+  });
+
+  test("renders a Modal with no title", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Modal trigger={<Button>Open modal</Button>} description="Modal description">
+        Modal content
+      </Modal>,
+    );
+
+    const trigger = screen.getByRole("button");
+    await user.click(trigger);
+
+    const dialog = screen.getByRole("dialog");
+    expect(dialog).not.toHaveTextContent("Modal title");
+    expect(dialog).toHaveTextContent("Modal description");
+    expect(dialog).toHaveTextContent("Modal content");
+  });
+
+  test("renders a Modal with no description", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Modal trigger={<Button>Open modal</Button>} title="Modal title">
+        Modal content
+      </Modal>,
+    );
+
+    const trigger = screen.getByRole("button");
+    await user.click(trigger);
+
+    const dialog = screen.getByRole("dialog");
+    expect(dialog).toHaveTextContent("Modal title");
+    expect(dialog).not.toHaveTextContent("Modal description");
+    expect(dialog).toHaveTextContent("Modal content");
+  });
+
+  test("renders a Modal with no title or description", async () => {
+    const user = userEvent.setup();
+
+    render(<Modal trigger={<Button>Open modal</Button>}>Modal content</Modal>);
+
+    const trigger = screen.getByRole("button");
+    await user.click(trigger);
+
+    const dialog = screen.getByRole("dialog");
+    expect(dialog).toHaveTextContent("Modal content");
+    expect(dialog).not.toHaveTextContent("Modal title");
+    expect(dialog).not.toHaveTextContent("Modal description");
+  });
+});
