f075753f81eefa52ab1eea4231611bedbe83a08b
John Szymanowski <github@john.szymanow.ski>
2025-05-14 14:53:43 +0800

    fix tests

--- tests/components/Separator.test.tsx
+++ tests/components/Separator.test.tsx
@@ -6,22 +6,22 @@ import { Separator } from "@/components";
 
 describe("Separator", () => {
   const EXPECTED_CLASS_NAME =
-    "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px";
+    "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px";
 
   test("renders a Separator", async () => {
     render(<Separator />);
 
     const separator = screen.getByRole("separator");
     expect(separator).toBeInTheDocument();
-    expect(separator).toHaveClass(EXPECTED_CLASS_NAME);
+    expect(separator.className).toBe(`${EXPECTED_CLASS_NAME} my-4`);
   });
 
-  test("renders with gap", async () => {
-    render(<Separator gap="4" />);
+  test("renders with custom gap", async () => {
+    render(<Separator gap="8" />);
 
     const separator = screen.getByRole("separator");
     expect(separator).toBeInTheDocument();
-    expect(separator).toHaveClass(`${EXPECTED_CLASS_NAME} my-4`);
+    expect(separator.className).toBe(`${EXPECTED_CLASS_NAME} my-8`);
   });
 
   test("renders a vertical Separator", async () => {
@@ -29,14 +29,14 @@ describe("Separator", () => {
 
     const separator = screen.getByRole("separator");
     expect(separator).toBeInTheDocument();
-    expect(separator).toHaveClass(EXPECTED_CLASS_NAME);
+    expect(separator.className).toBe(`${EXPECTED_CLASS_NAME} mx-2`);
   });
 
-  test("renders a vertical Separator with gap", async () => {
-    render(<Separator gap="4" />);
+  test("renders a vertical Separator with custom gap", async () => {
+    render(<Separator orientation="vertical" gap="4" />);
 
     const separator = screen.getByRole("separator");
     expect(separator).toBeInTheDocument();
-    expect(separator).toHaveClass(`${EXPECTED_CLASS_NAME} my-4`);
+    expect(separator.className).toBe(`${EXPECTED_CLASS_NAME} mx-4`);
   });
 });
