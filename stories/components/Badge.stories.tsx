0cc1a1b76447982d73745c277b5b2a19b427a99a
John Szymanowski <github@john.szymanow.ski>
2025-05-15 11:35:03 +0800

    clean up stories

--- stories/components/Badge.stories.tsx
+++ stories/components/Badge.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Badge } from "@/components";
-import { Flex } from "@/layout";
+import { Flex, Grid } from "@/layout";
 
 const meta: Meta<typeof Badge> = {
   title: "Components/Badge",
@@ -29,22 +29,35 @@ export const Default: Story = {
   args: defaultArgs,
 };
 
-export const Secondary: Story = {
-  args: { ...defaultArgs, variant: "secondary" },
-};
-
-export const Destructive: Story = {
-  args: { ...defaultArgs, variant: "destructive" },
-};
-
-export const Outline: Story = {
-  args: { ...defaultArgs, variant: "outline" },
+export const Variants: Story = {
+  render: () => (
+    <Flex gap="2">
+      <Badge label="Default" />
+      <Badge label="Secondary" variant="secondary" />
+      <Badge label="Destructive" variant="destructive" />
+      <Badge label="Outline" variant="outline" />
+    </Flex>
+  ),
 };
 
 export const Clickable: Story = {
-  args: { ...defaultArgs, onClick: () => alert("Clicked") },
+  render: () => (
+    <Flex gap="2">
+      <Badge label="Default" onClick={() => alert("Default")} />
+      <Badge label="Secondary" variant="secondary" onClick={() => alert("Secondary")} />
+      <Badge label="Destructive" variant="destructive" onClick={() => alert("Destructive")} />
+      <Badge label="Outline" variant="outline" onClick={() => alert("Outline")} />
+    </Flex>
+  ),
 };
 
 export const CustomClassName: Story = {
-  args: { ...defaultArgs, className: "font-mono font-thin text-xl" },
+  render: () => (
+    <Flex gap="2">
+      <Badge label="Default" className="font-mono uppercase font-thin text-xl" />
+      <Badge label="Secondary" variant="secondary" className="font-mono uppercase font-thin text-xl" />
+      <Badge label="Destructive" variant="destructive" className="font-mono uppercase font-thin text-xl" />
+      <Badge label="Outline" variant="outline" className="font-mono uppercase font-thin text-xl" />
+    </Flex>
+  ),
 };
