8c816025f92a906de2350cd2d000e2df9f1aad85
John Szymanowski <github@john.szymanow.ski>
2025-06-02 19:25:35 +0800

    lint

--- stories/components/Alert.stories.tsx
+++ stories/components/Alert.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-
-import { Alert } from "@/components/Alert";
 import type { AlertProps } from "@/components/Alert";
+import { Alert } from "@/components/Alert";
 
 const meta: Meta<typeof Alert> = {
   title: "Components/Alert",
