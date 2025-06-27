79b316f9945adf20c3f550f33269c0a9eb833aa8
John Szymanowski <github@john.szymanow.ski>
2025-06-27 09:24:40 +0800

    Make Container flx by default

--- lib/layout/Container/Container.tsx
+++ lib/layout/Container/Container.tsx
@@ -1,15 +1,30 @@
-import { Box, type BoxProps } from "@/layout";
+import { Box, Flex, type FlexProps } from "@/layout";
 import { cn } from "@/utils";
 
-export interface ContainerProps extends BoxProps {
+type Display = "block" |"flex";
+
+export interface ContainerProps extends FlexProps {
   children: React.ReactNode;
+  display?: Display;
   className?: string;
 }
 
 export const Container = ({ children, className, ...props }: ContainerProps) => {
+  const { display = "flex", ...rest } = props;
+
+  const classes = cn("w-full max-w-6xl px-4 py-2 md:px-8 md:py-4 lg:px-12 lg:py-6", className);
+
+  if (display === "block") {
+    return (
+      <Box className={classes} {...rest}>
+        {children}
+      </Box>
+    );
+  }
+
   return (
-    <Box width="full" className={cn("max-w-6xl px-4 py-2 md:px-8 md:py-4 lg:px-12 lg:py-6", className)} {...props}>
+    <Flex className={classes} {...rest}>
       {children}
-    </Box>
+    </Flex>
   );
 };
--- tests/layout/Container.test.tsx
+++ tests/layout/Container.test.tsx
@@ -5,8 +5,18 @@ import "@testing-library/jest-dom";
 import { Container } from "@/layout";
 
 describe("Container", () => {
-  it("renders with default props", () => {
+  it("renders as flex by default", () => {
     render(<Container data-testid="container-element">Content</Container>);
+    const element = screen.getByTestId("container-element");
+
+    expect(element.tagName).toBe("DIV");
+    expect(element).toBeInTheDocument();
+    expect(element.className).toBe("flex w-full max-w-6xl px-4 py-2 md:px-8 md:py-4 lg:px-12 lg:py-6");
+    expect(element).toHaveTextContent("Content");
+  });
+
+  it("renders as block if specified", () => {
+    render(<Container display="block" data-testid="container-element">Content</Container>);
     const element = screen.getByTestId("container-element");
 
     expect(element.tagName).toBe("DIV");
@@ -22,7 +32,7 @@ describe("Container", () => {
       </Container>,
     );
     const element = screen.getByTestId("container-element");
-    expect(element.className).toBe("w-full max-w-6xl md:px-8 md:py-4 lg:px-12 lg:py-6 p-8");
+    expect(element.className).toBe("flex w-full max-w-6xl md:px-8 md:py-4 lg:px-12 lg:py-6 p-8");
   });
 
   it("renders with custom element type", () => {
