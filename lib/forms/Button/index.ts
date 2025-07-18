f003a84edff6e2b3e15589fa248a42878208dfd2
John Szymanowski <github@john.szymanow.ski>
2025-07-17 22:36:01 -0400

    Export Button props

--- lib/forms/Button/index.ts
+++ lib/forms/Button/index.ts
@@ -1,6 +1,4 @@
-export type { ButtonProps } from "./Button";
-export { Button } from "./Button";
-export type { ButtonGroupProps } from "./ButtonGroup";
-export { ButtonGroup } from "./ButtonGroup";
-export { DeleteButton } from "./DeleteButton";
-export { SubmitButton } from "./SubmitButton";
+export * from "./Button";
+export * from "./ButtonGroup";
+export * from "./DeleteButton";
+export * from "./SubmitButton";
