b6eb47cbdcb1ef9cb7584a6baebfd2c99ae40c2e
John Szymanowski <github@john.szymanow.ski>
2025-08-27 08:53:59 +0800

    add state to story

--- lib/components/ThemeSelector/ThemeSelector.tsx
+++ lib/components/ThemeSelector/ThemeSelector.tsx
@@ -1,6 +1,8 @@
-import { MonitorCog, Moon, Sun } from "lucide-react";
+import { Computer, MonitorCog, Moon, Sun } from "lucide-react";
 import { Tooltip } from "@/components";
+import { Button } from "@/forms";
 import { Flex } from "@/layout";
+import { cn } from "@/utils";
 
 export type Theme = "system" | "light" | "dark";
 
@@ -10,10 +12,55 @@ export interface ThemeSelectorProps {
   setTheme: (theme: Theme) => void;
 }
 
+const StackedThemes = [
+  {
+    theme: "light",
+    icon: <Sun />,
+    label: "Light mode",
+  },
+  {
+    theme: "dark",
+    icon: <Moon />,
+    label: "Dark mode",
+  },
+  {
+    theme: "system",
+    icon: <Computer />,
+    label: "System theme",
+  },
+];
+
+const StackedThemeSelector = ({ theme, setTheme }: ThemeSelectorProps) => {
+  const toggleTheme = () => {
+    const nextTheme = theme === "light" ? "dark" : theme === "dark" ? "system" : "light";
+    setTheme(nextTheme);
+  };
+
+  return (
+    <Flex gap="4">
+      {StackedThemes.map((t) => (
+        <Button
+          className={cn("hidden", t.theme === theme && "block")}
+          icon={t.icon}
+          key={t.theme}
+          onClick={toggleTheme}
+          variant="ghost"
+        >
+          {t.label}
+        </Button>
+      ))}
+    </Flex>
+  );
+};
+
 export const ThemeSelector = ({ variant = "horizontal", theme, setTheme }: ThemeSelectorProps) => {
   const getClassName = (prospectiveTheme: Theme) => {
     return theme === prospectiveTheme ? "text-primary/75" : "text-primary/25 hover:text-primary";
   };
+
+  if (variant === "stacked") {
+    return <StackedThemeSelector setTheme={setTheme} theme={theme} />;
+  }
 
   return (
     <Flex gap="4">
--- stories/components/ThemeSelector.stories.tsx
+++ stories/components/ThemeSelector.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { useState } from "react";
 import { type Theme, ThemeSelector, type ThemeSelectorProps } from "@/components";
+import { Box } from "@/layout";
 
 const meta: Meta<typeof ThemeSelector> = {
   title: "Components/ThemeSelector",
@@ -11,21 +13,33 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+let theme: Theme = "system";
+
 const defaultArgs: ThemeSelectorProps = {
-  theme: "system",
-  setTheme: (prospectTheme: Theme) => {
-    window.alert(`Setting theme to ${prospectTheme}`);
+  theme,
+  setTheme: (prospectiveTheme: Theme) => {
+    theme = prospectiveTheme;
   },
 };
 
+const DecoratedThemeSelector = (props: ThemeSelectorProps) => {
+  const [theme, setTheme] = useState<Theme>(props.theme);
+
+  return (
+    <Box className="w-48">
+      <ThemeSelector {...props} setTheme={setTheme} theme={theme} />
+    </Box>
+  );
+};
+
 export const Default: Story = {
-  render: () => <ThemeSelector {...defaultArgs} />,
+  render: () => <DecoratedThemeSelector {...defaultArgs} />,
 };
 
 export const Horizontal: Story = {
-  render: () => <ThemeSelector {...defaultArgs} variant="horizontal" />,
+  render: () => <DecoratedThemeSelector {...defaultArgs} variant="horizontal" />,
 };
 
 export const Stacked: Story = {
-  render: () => <ThemeSelector {...defaultArgs} variant="stacked" />,
+  render: () => <DecoratedThemeSelector {...defaultArgs} variant="stacked" />,
 };
