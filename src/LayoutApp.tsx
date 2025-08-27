f88a3f791bbed497e3b50bd91aba14100bc91131
John Szymanowski <github@john.szymanow.ski>
2025-08-27 08:30:19 +0800

    show dimensions

--- src/LayoutApp.tsx
+++ src/LayoutApp.tsx
@@ -1,26 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router";
 import { Button } from "@/forms";
 import { Container, Layout } from "@/layout";
+import { cn } from "@/utils";
 import { Brand } from "./components/Brand";
 import { FooterContent } from "./components/FooterContent";
-import { useTheme } from "./context/useTheme";
 
 export default function App() {
-  const { theme, setTheme } = useTheme();
+  const [showDimensions, setShowDimensions] = useState(true);
 
   return (
     <Layout>
-      <Layout.Header>
+      <Layout.Header className={cn(showDimensions && "bg-red-500/5 inset-shadow-sm inset-shadow-red-500")}>
         <Brand />
       </Layout.Header>
-      <Layout.Body>
+      <Layout.Body className={cn(showDimensions && "bg-blue-500/5 inset-shadow-sm inset-shadow-blue-500")}>
         <Container>
           <Button asChild fit>
             <Link to="/">Home</Link>
           </Button>
         </Container>
+        <Container>
+          <Button fit onClick={() => setShowDimensions(!showDimensions)} variant="outline">
+            {showDimensions ? "Hide " : "Show "} dimensions
+          </Button>
+        </Container>
       </Layout.Body>
-      <Layout.Footer>
+      <Layout.Footer className={cn(showDimensions && "bg-green-500/10 inset-shadow-sm inset-shadow-green-500")}>
         <FooterContent />
       </Layout.Footer>
     </Layout>
