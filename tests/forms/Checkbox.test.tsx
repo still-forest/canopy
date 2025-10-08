85f51c6683b5507569998bfcd26a07b357f607d0
John Szymanowski <github@john.szymanow.ski>
2025-10-08 17:20:26 +0800

    Checkbox

--- tests/forms/Checkbox.test.tsx
+++ tests/forms/Checkbox.test.tsx
@@ -26,7 +26,7 @@ describe("Checkbox", () => {
     const label = screen.getByText("Test Checkbox");
     expect(label).toHaveAttribute("for", "test-checkbox");
     expect(label.className).toBe(
-      "flex select-none items-center gap-2 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 font-display font-normal text-base text-foreground cursor-pointer",
+      "flex items-center gap-2 select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 font-display font-normal text-base text-foreground cursor-pointer",
     );
 
     fireEvent.click(checkbox);
