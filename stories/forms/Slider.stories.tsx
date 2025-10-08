77c64c684bf62272ad179cd5ece16d061dfab931
John Szymanowski <github@john.szymanow.ski>
2025-10-08 19:19:25 +0800

    values doesn't exist

--- stories/forms/Slider.stories.tsx
+++ stories/forms/Slider.stories.tsx
@@ -18,13 +18,20 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const defaultProps = {
-  values: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-};
+const defaultProps = {};
 
 export const Default: Story = {
   args: defaultProps,
 };
+
+export const WithDefaultValue: Story = {
+  args: { ...defaultProps, defaultValue: [20] },
+};
+
+export const WithMultipleDefaultValues: Story = {
+  args: { ...defaultProps, defaultValue: [20, 60] },
+};
+
 export const Vertical: Story = {
   args: { ...defaultProps, orientation: "vertical" },
 };
