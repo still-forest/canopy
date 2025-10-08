1edaef55bd87c177dc21ed174a758e33ccce1322
John Szymanowski <github@john.szymanow.ski>
2025-10-08 21:30:05 +0800

    pass through class names

--- lib/forms/Slider/Slider.tsx
+++ lib/forms/Slider/Slider.tsx
@@ -3,24 +3,36 @@ import { cn } from "@/utils";
 
 interface SliderProps extends React.ComponentProps<typeof BaseSlider> {
   size?: "xs" | "sm" | "md" | "lg" | "xl";
+  thumbClassName?: string;
+  trackClassName?: string;
 }
 
-export const Slider = ({ size = "md", ...props }: SliderProps) => (
-  <BaseSlider
-    thumbClassName={cn({
-      "size-2": size === "xs",
-      "size-3": size === "sm",
-      "size-4": size === "md",
-      "size-5": size === "lg",
-      "size-6": size === "xl",
-    })}
-    trackClassName={cn({
-      "data-[orientation=horizontal]:h-1 data-[orientation=vertical]:w-1": size === "xs",
-      "data-[orientation=horizontal]:h-1.5 data-[orientation=vertical]:w-1.5": size === "sm",
-      "data-[orientation=horizontal]:h-2 data-[orientation=vertical]:w-2": size === "md",
-      "data-[orientation=horizontal]:h-3 data-[orientation=vertical]:w-3": size === "lg",
-      "data-[orientation=horizontal]:h-4 data-[orientation=vertical]:w-4": size === "xl",
-    })}
-    {...props}
-  />
-);
+export const Slider = ({ size = "md", thumbClassName, trackClassName, ...props }: SliderProps) => {
+  console.log("props", props);
+  return (
+    <BaseSlider
+      thumbClassName={cn(
+        {
+          "size-2": size === "xs",
+          "size-3": size === "sm",
+          "size-4": size === "md",
+          "size-5": size === "lg",
+          "size-6": size === "xl",
+        },
+        "bg-primary",
+        thumbClassName,
+      )}
+      trackClassName={cn(
+        {
+          "data-[orientation=horizontal]:h-1 data-[orientation=vertical]:w-1": size === "xs",
+          "data-[orientation=horizontal]:h-1.5 data-[orientation=vertical]:w-1.5": size === "sm",
+          "data-[orientation=horizontal]:h-2 data-[orientation=vertical]:w-2": size === "md",
+          "data-[orientation=horizontal]:h-3 data-[orientation=vertical]:w-3": size === "lg",
+          "data-[orientation=horizontal]:h-4 data-[orientation=vertical]:w-4": size === "xl",
+        },
+        trackClassName,
+      )}
+      {...props}
+    />
+  );
+};
