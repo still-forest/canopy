215a11de4d6ef298adca507db77d906adbb8cf11
John Szymanowski <github@john.szymanow.ski>
2025-10-08 11:38:52 +0800

    lint

--- stories/forms/CopyButton.stories.tsx
+++ stories/forms/CopyButton.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { PiggyBank } from "lucide-react";
 
 import { CopyButton } from "@/forms";
 import { sampleParagraphText } from "../support/sampleText";
@@ -15,106 +14,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const defaultProps = {
-  content: sampleParagraphText,
+  content: sampleParagraphText.join("\n\n"),
 };
 
 export const Default: Story = {
   args: {
     ...defaultProps,
-  },
-};
-
-export const ExtraSmall: Story = {
-  args: {
-    ...defaultProps,
-    size: "xs",
-  },
-};
-
-export const Small: Story = {
-  args: {
-    ...defaultProps,
-    size: "sm",
-  },
-};
-
-export const Medium: Story = {
-  args: {
-    ...defaultProps,
-    size: "md",
-  },
-};
-
-export const Large: Story = {
-  args: {
-    ...defaultProps,
-    size: "lg",
-  },
-};
-
-export const Submitting: Story = {
-  args: {
-    ...defaultProps,
-    submitting: true,
-  },
-};
-
-export const Disabled: Story = {
-  args: {
-    ...defaultProps,
-    disabled: true,
-  },
-};
-
-export const Submit: Story = {
-  args: {
-    ...defaultProps,
-    action: "submit",
-  },
-};
-
-export const Save: Story = {
-  args: {
-    ...defaultProps,
-    action: "save",
-  },
-};
-
-export const Send: Story = {
-  args: {
-    ...defaultProps,
-    action: "send",
-  },
-};
-
-export const NoIcon: Story = {
-  args: {
-    ...defaultProps,
-    noIcon: true,
-  },
-};
-
-export const WithCustomSubmittingIcon: Story = {
-  args: {
-    ...defaultProps,
-    submittingIcon: PiggyBank,
-    submitting: true,
-  },
-};
-
-export const WithCustomLabels: Story = {
-  args: {
-    ...defaultProps,
-    label: "Do the thing",
-    submittingLabel: "Doing it",
-  },
-};
-
-export const WithCustomLabelsSubmitting: Story = {
-  args: {
-    ...defaultProps,
-    label: "Do the thing",
-    submittingLabel: "Doing it",
-    submitting: true,
   },
 };
