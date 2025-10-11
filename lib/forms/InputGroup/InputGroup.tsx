2705c19a62312bd01143f887180abbc6e8dbb6df
John Szymanowski <github@john.szymanow.ski>
2025-10-11 10:10:26 +0800

    use my Button

--- lib/forms/InputGroup/InputGroup.tsx
+++ lib/forms/InputGroup/InputGroup.tsx
@@ -2,26 +2,24 @@ import type { ReactNode } from "react";
 import {
   InputGroup as BaseInputGroup,
   InputGroupAddon,
-  type InputGroupButton,
   InputGroupInput,
   InputGroupText,
   InputGroupTextarea,
 } from "@/components/ui/input-group";
-import { Button } from "@/forms/Button";
+import { Button, type ButtonProps } from "@/forms/Button";
 
 export type InputGroupProps = React.ComponentProps<typeof BaseInputGroup> & {
   children: ReactNode;
 };
 type InputGroupInputProps = React.ComponentProps<typeof InputGroupInput>;
 type InputGroupAddonProps = React.ComponentProps<typeof InputGroupAddon>;
-type InputGroupButtonProps = React.ComponentProps<typeof InputGroupButton>;
 type InputGroupTextProps = React.ComponentProps<typeof InputGroupText>;
 type InputGroupTextareaProps = React.ComponentProps<typeof InputGroupTextarea>;
 
 type InputGroupComponent = React.FC<InputGroupProps> & {
   Input: React.FC<InputGroupInputProps>;
   Addon: React.FC<InputGroupAddonProps>;
-  Button: React.FC<InputGroupButtonProps>;
+  Button: React.FC<ButtonProps>;
   Text: React.FC<InputGroupTextProps>;
   Textarea: React.FC<InputGroupTextareaProps>;
 };
