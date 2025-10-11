551b9a5e627f060e439f86176c6ad2f144254267
John Szymanowski <github@john.szymanow.ski>
2025-10-11 11:01:09 +0800

    lint

--- stories/forms/InputGroup.stories.tsx
+++ stories/forms/InputGroup.stories.tsx
@@ -36,7 +36,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: () => (
     <InputGroup>
-      <InputGroup.Input placeholder="Search..." />
+      <InputGroup.Input name="search" placeholder="Search..." />
       <InputGroup.Addon>
         <SearchIcon />
       </InputGroup.Addon>
