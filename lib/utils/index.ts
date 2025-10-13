5ea93f6db1406b818569e2a457f12c45051bbb34
John Szymanowski <github@john.szymanow.ski>
2025-10-13 19:27:36 +0800

    cleanup

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,2 @@
 export type { Breakpoint, ResponsiveValue } from "@/types/layout";
 export { cn } from "./cn";
-export { getResponsiveClasses, getResponsiveConditionalClasses, isResponsiveValue } from "./responsive";
