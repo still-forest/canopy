89202a30ad4a1a84c9d563c83633d862256c0fde
John Szymanowski <github@john.szymanow.ski>
2025-11-12 21:59:29 +0800

    tweak: Export all types

--- lib/main.ts
+++ lib/main.ts
@@ -3,6 +3,6 @@ export * from "./forms";
 export * from "./interstitials";
 export * from "./layout";
 export * from "./navigation";
-export type { Theme } from "./types";
+export * from "./types";
 export * from "./typography";
 export * from "./utils";
