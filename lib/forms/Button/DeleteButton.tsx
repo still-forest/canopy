6779cce307ad621a53420dfeb2a025fcb438bafa
John Szymanowski <github@john.szymanow.ski>
2025-11-24 07:18:08 +0800

    DeleteButon too

--- lib/forms/Button/DeleteButton.tsx
+++ lib/forms/Button/DeleteButton.tsx
@@ -1,17 +1,21 @@
 import { Trash2 } from "lucide-react";
+import type { ReactElement } from "react";
 
 import { Button, type ButtonProps } from "@/forms";
 
 interface Props extends ButtonProps {
+  icon?: ReactElement;
   disabled?: boolean;
   handleDelete: () => void;
 }
 
-export const DeleteButton = ({ disabled = false, handleDelete, ...rest }: Props) => {
+export const DeleteButton = ({ icon, disabled = false, handleDelete, ...rest }: Props) => {
+  const IconComponent = icon ? () => icon : Trash2;
+
   return (
     <Button
       disabled={disabled}
-      icon={<Trash2 />}
+      icon={<IconComponent />}
       onClick={handleDelete}
       variant="outline"
       {...rest}
