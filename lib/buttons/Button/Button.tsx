f2e2a70e910a44f4a208f489dc9c081f074abea5
John Szymanowski <github@john.szymanow.ski>
2026-02-22 22:43:40 +0800

    Remove svg constraint on button icon

--- lib/buttons/Button/Button.tsx
+++ lib/buttons/Button/Button.tsx
@@ -1,7 +1,5 @@
 import type { ComponentProps } from "react";
 import "./Button.css";
-import { cloneElement } from "react";
-import { cn } from "@/utils/cn";
 import type { BaseButtonProps } from "./types";
 import { buildButtonClassNames } from "./utils";
 
@@ -25,16 +23,18 @@ export const Button = ({
   if (label && children) {
     throw new Error("Both label and children should not be provided");
   }
+
   if (!label && !children && !icon) {
     throw new Error("Either label or children or icon must be provided");
   }
+
   return (
     <button
       className={buildButtonClassNames({ variant, size, outline, knockout, rounded, fit, asIcon, className })}
       type={type}
       {...props}
     >
-      {icon && cloneElement(icon, { className: cn("btn-icon", icon.props.className) })}
+      {icon}
       {!asIcon && (label || children)}
     </button>
   );
