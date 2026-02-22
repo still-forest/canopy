ca0acecf34e7975a0a526d3ec1e80c6edbf6c572
John Szymanowski <github@john.szymanow.ski>
2026-02-22 21:26:12 +0800

    CopyButton

--- lib/buttons/CopyButton/CopyButton.tsx
+++ lib/buttons/CopyButton/CopyButton.tsx
@@ -1,13 +1,12 @@
 import { Clipboard, ClipboardCheck } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
-import { cn } from "@/utils";
 import { Button, type ButtonProps } from "../Button";
 
 interface CopyButtonProps extends Omit<ButtonProps, "onClick" | "disabled"> {
   content: string;
 }
 
-export const CopyButton = ({ content, className, size = "md", ...props }: CopyButtonProps) => {
+export const CopyButton = ({ content, ...props }: CopyButtonProps) => {
   const [recentlyCopied, setRecentlyCopied] = useState(false);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
@@ -43,22 +42,10 @@ export const CopyButton = ({ content, className, size = "md", ...props }: CopyBu
 
   return (
     <Button
-      className={cn(
-        {
-          "w-[88px]": size === "xs",
-          "w-[96px]": size === "sm",
-          "w-[100px]": size === "md",
-          "w-[125px]": size === "lg",
-          "w-[144px]": size === "xl",
-        },
-        "justify-start",
-        className,
-      )}
       disabled={recentlyCopied}
       icon={recentlyCopied ? <ClipboardCheck /> : <Clipboard />}
       label={recentlyCopied ? "Copied" : "Copy"}
       onClick={copyText}
-      size={size}
       {...props}
     />
   );
--- stories/buttons/CopyButton.stories.tsx
+++ stories/buttons/CopyButton.stories.tsx
@@ -1,11 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { CopyButton } from "@/buttons";
-import { Flex } from "@/layout";
 import { sampleParagraphText } from "../support/sampleText";
 
 const meta: Meta<typeof CopyButton> = {
-  title: "Buttons/Copy",
+  title: "Buttons/CopyButton",
   component: CopyButton,
   tags: ["autodocs"],
 } satisfies Meta<typeof CopyButton>;
@@ -26,13 +25,28 @@ export const Default: Story = {
 
 export const Sizes: Story = {
   render: () => (
-    <Flex direction="col" gap="2">
-      <CopyButton {...defaultProps} size="xs" />
-      <CopyButton {...defaultProps} size="sm" />
-      <CopyButton {...defaultProps} size="md" />
-      <CopyButton {...defaultProps} size="lg" />
-      <CopyButton {...defaultProps} size="xl" />
-    </Flex>
+    <div className="grid grid-cols-5 gap-4">
+      <div className="flex flex-col items-center gap-2">
+        <p className="footnote">xs</p>
+        <CopyButton {...defaultProps} size="xs" />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <p className="footnote">sm</p>
+        <CopyButton {...defaultProps} size="sm" />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <p className="footnote">md</p>
+        <CopyButton {...defaultProps} size="md" />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <p className="footnote">lg</p>
+        <CopyButton {...defaultProps} size="lg" />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <p className="footnote">xl</p>
+        <CopyButton {...defaultProps} size="xl" />
+      </div>
+    </div>
   ),
 };
 
