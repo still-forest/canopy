fc866b58356368b86ff596326ce41a0ba8e53fbb
John Szymanowski <github@john.szymanow.ski>
2026-02-23 11:20:10 +0800

    update container

--- tests/layout/Container.test.tsx
+++ tests/layout/Container.test.tsx
@@ -11,74 +11,7 @@ describe("Container", () => {
 
     expect(element.tagName).toBe("DIV");
     expect(element).toBeInTheDocument();
-    expect(element.className).toContain("flex");
-    expect(element.className).toContain("flex-col");
-    expect(element.className).toContain("w-full");
-    expect(element).toHaveTextContent("Content");
-  });
-
-  it("renders with no separation", () => {
-    render(
-      <Container data-testid="container-element" separation="none">
-        Content
-      </Container>,
-    );
-    const element = screen.getByTestId("container-element");
-    expect(element.className).toContain("py-0");
-  });
-
-  it("renders with extra small separation", () => {
-    render(
-      <Container data-testid="container-element" separation="xs">
-        Content
-      </Container>,
-    );
-    const element = screen.getByTestId("container-element");
-    expect(element.className).toContain("py-0.5");
-  });
-
-  it("renders with small separation", () => {
-    render(
-      <Container data-testid="container-element" separation="sm">
-        Content
-      </Container>,
-    );
-    const element = screen.getByTestId("container-element");
-    expect(element.className).toContain("py-1");
-  });
-
-  it("renders with medium separation", () => {
-    render(
-      <Container data-testid="container-element" separation="md">
-        Content
-      </Container>,
-    );
-    const element = screen.getByTestId("container-element");
-    expect(element.className).toContain("py-2");
-  });
-
-  it("renders with large separation", () => {
-    render(
-      <Container data-testid="container-element" separation="lg">
-        Content
-      </Container>,
-    );
-    const element = screen.getByTestId("container-element");
-    expect(element.className).toContain("py-4");
-  });
-
-  it("renders as block if specified", () => {
-    render(
-      <Container data-testid="container-element" display="block">
-        Content
-      </Container>,
-    );
-    const element = screen.getByTestId("container-element");
-
-    expect(element.tagName).toBe("DIV");
-    expect(element).toBeInTheDocument();
-    expect(element.className).toContain("w-full");
-    expect(element.className).not.toContain("flex");
+    expect(element.className).toBe("layout-container");
     expect(element).toHaveTextContent("Content");
   });
 
