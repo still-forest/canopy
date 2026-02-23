8a9f7262e73b5aa1b17e8fffc8f83840abbd57a8
John Szymanowski <github@john.szymanow.ski>
2026-02-23 18:35:03 +0800

    feat(table): Rebuild Table (#440)

--- lib/components/Table/Table.tsx
+++ lib/components/Table/Table.tsx
@@ -1,34 +1,45 @@
-import type * as React from "react";
-import {
-  Table as BaseTable,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import type { ComponentProps } from "react";
+import { cn } from "@/utils/cn";
+import "./Table.css";
 
-export { TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-
-type TableComponent = React.FC<React.ComponentProps<"table">> & {
-  Body: typeof TableBody;
-  Caption: typeof TableCaption;
-  Cell: typeof TableCell;
-  Head: typeof TableHead;
-  Header: typeof TableHeader;
-  Row: typeof TableRow;
+const Table = ({ className, ...props }: ComponentProps<"table">) => {
+  return <table className={cn("table", className)} {...props} />;
 };
 
-const Table: TableComponent = ({ children, ...props }: React.ComponentProps<"table">) => {
-  return <BaseTable {...props}>{children}</BaseTable>;
+const TableHeader = (props: ComponentProps<"thead">) => {
+  return <thead {...props} />;
 };
 
-Table.Body = TableBody;
-Table.Caption = TableCaption;
-Table.Cell = TableCell;
-Table.Head = TableHead;
+const TableBody = (props: ComponentProps<"tbody">) => {
+  return <tbody {...props} />;
+};
+
+const TableFooter = (props: ComponentProps<"tfoot">) => {
+  return <tfoot {...props} />;
+};
+
+const TableHead = (props: ComponentProps<"th">) => {
+  return <th {...props} />;
+};
+
+const TableRow = (props: ComponentProps<"tr">) => {
+  return <tr {...props} />;
+};
+
+const TableCell = (props: ComponentProps<"td">) => {
+  return <td {...props} />;
+};
+
+const TableCaption = (props: ComponentProps<"caption">) => {
+  return <caption {...props} />;
+};
+
 Table.Header = TableHeader;
+Table.Body = TableBody;
+Table.Footer = TableFooter;
+Table.Head = TableHead;
 Table.Row = TableRow;
+Table.Cell = TableCell;
+Table.Caption = TableCaption;
 
 export { Table };
