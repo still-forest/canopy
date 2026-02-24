aa9270ab690cde7ab2349e684975c6672a9bca59
John Szymanowski <github@john.szymanow.ski>
2026-02-25 07:03:27 +0800

    nativeselect

--- lib/forms/fields/NativeSelectField/NativeSelectField.tsx
+++ lib/forms/fields/NativeSelectField/NativeSelectField.tsx
@@ -55,7 +55,7 @@ const NativeSelectField = ({
     size === "md" && "h-9 text-sm",
     size === "lg" && "h-10 text-base",
     size === "xl" && "h-11 text-lg",
-    "bg-background",
+    "bg-background border-input-border bg-input dark:bg-input",
     className,
   );
 
