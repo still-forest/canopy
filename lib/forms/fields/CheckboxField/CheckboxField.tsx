ae4d5bfc4c8086a5409f2646b90154a0eb7671cb
John Szymanowski <github@john.szymanow.ski>
2026-02-25 18:00:07 +0800

    use Field

--- lib/forms/fields/CheckboxField/CheckboxField.tsx
+++ lib/forms/fields/CheckboxField/CheckboxField.tsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
 
 import { Hint } from "@/components";
-import { InputError, Label } from "@/forms";
+import { Field } from "@/forms";
 import { Checkbox as BaseCheckbox, type CheckboxProps } from "@/forms/inputs";
-import { Flex } from "@/layout";
-import { Text } from "@/typography";
-import { cn } from "@/utils";
 
 export interface CheckboxFieldProps extends CheckboxProps {
-  label: string;
+  name: string;
+  label?: string;
   labelClassName?: string;
   hint?: string;
   note?: string;
@@ -47,32 +45,28 @@ const CheckboxField = ({
   };
 
   return (
-    <Flex direction="col">
-      <Flex align="center" gap="2">
-        <BaseCheckbox
-          checked={checkedState}
-          id={fullId}
-          name={name}
-          onCheckedChange={handleChange}
-          value={effectiveValue}
-          {...props}
-        />
-        <Label className={cn("cursor-pointer", labelClassName)} htmlFor={fullId}>
-          {label}
-        </Label>
-        {hint && <Hint content={hint} />}
-      </Flex>
-      {(note || error) && (
-        <Flex className="ml-6" direction="col" gap="2">
-          {note && (
-            <Text size="sm" variant="muted">
-              {note}
-            </Text>
-          )}
-          {error && <InputError message={error} />}
-        </Flex>
-      )}
-    </Flex>
+    <Field orientation="horizontal">
+      <BaseCheckbox
+        checked={checkedState}
+        id={fullId}
+        name={name}
+        onCheckedChange={handleChange}
+        value={effectiveValue}
+        {...props}
+      />
+      <Field.Content>
+        {label && (
+          <Field.LabelGroup>
+            <Field.Label className={labelClassName} htmlFor={fullId}>
+              {label}
+              {hint && <Hint content={hint} />}
+            </Field.Label>
+          </Field.LabelGroup>
+        )}
+        {note && <Field.Description>{note}</Field.Description>}
+        {error && <Field.Error>{error}</Field.Error>}
+      </Field.Content>
+    </Field>
   );
 };
 
