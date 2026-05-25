2385f8f80c66f8cf571d8fab3ef2550d835b9280
John Szymanowski <github@john.szymanow.ski>
2026-05-25 08:15:47 +0800

    rebuild with combobox (wip)

--- lib/forms/inputs/MultiSelectInput/MultiSelectInput.tsx
+++ lib/forms/inputs/MultiSelectInput/MultiSelectInput.tsx
@@ -1,11 +1,19 @@
-import { ChevronDownIcon, Square, SquareCheck } from "lucide-react";
 import { useMemo } from "react";
 import { Button } from "@/buttons";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Separator } from "@/components/ui/separator";
+import {
+  Combobox,
+  ComboboxChip,
+  ComboboxChips,
+  ComboboxChipsInput,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxItem,
+  ComboboxList,
+  ComboboxValue,
+  useComboboxAnchor,
+} from "@/components/ui/combobox";
 import { Checkbox } from "@/forms/inputs";
 import { Flex } from "@/layout";
-import { cn } from "@/utils/cn";
 import type { SelectOption, SelectOptionGroup } from "../types";
 import "./MultiSelectInput.css";
 
@@ -29,19 +37,21 @@ export const MultiSelectInput = ({
   size = "default",
   className,
 }: MultiSelectInputProps) => {
+  const anchor = useComboboxAnchor();
+
   const isOptionGroup = useMemo(() => options.some((option) => "options" in option), [options]);
   const flatOptions: SelectOption[] = useMemo(() => {
     return isOptionGroup
       ? (options as SelectOptionGroup[]).flatMap((optionGroup) => optionGroup.options)
       : (options as SelectOption[]);
   }, [options, isOptionGroup]);
-  const optionGroups: SelectOptionGroup[] = isOptionGroup
+  const _optionGroups: SelectOptionGroup[] = isOptionGroup
     ? (options as SelectOptionGroup[])
     : [{ label: null, options: options as SelectOption[] }];
 
   const hasFilter = flatOptions.length !== selectedOptions.length;
 
-  const renderOption = ({ value, label }: SelectOption) => (
+  const _renderOption = ({ value, label }: SelectOption) => (
     <Flex align="center" className="group" direction="row" gap="2" key={value}>
       <Checkbox
         checked={selectedOptions.includes(value)}
@@ -64,49 +74,82 @@ export const MultiSelectInput = ({
     </Flex>
   );
 
-  const displayLabel = hasFilter
+  const _displayLabel = hasFilter
     ? filteredLabel || `${selectedOptions.length} of ${flatOptions.length} selected`
     : unFilteredLabel;
 
   return (
-    <Popover>
-      <PopoverTrigger
-        className={cn(
-          "multi-select-trigger",
-          `multi-select-trigger--${size}`,
-          hasFilter && "multi-select-trigger--filtered",
-          className,
-        )}
-      >
-        <span className={cn("multi-select-label", hasFilter && "multi-select-label--filtered")}>{displayLabel}</span>
-        <ChevronDownIcon className="multi-select-chevron" />
-      </PopoverTrigger>
-      <PopoverContent className="multi-select-content w-(--anchor-width)">
-        <Flex className="multi-select-options" direction="col" gap="4">
-          {optionGroups.map((optionGroup, _index) => (
-            <Flex direction="col" gap="1" key={optionGroup.label ?? "options"}>
-              {optionGroup.label && <p className="multi-select-group-label">{optionGroup.label}</p>}
-              {optionGroup.options.map(renderOption)}
-            </Flex>
-          ))}
-        </Flex>
-        <Separator />
-        <Flex direction="row" gap="2">
-          <Button
-            onClick={() => onChange(flatOptions.map(({ value }) => value))}
-            outline={hasFilter}
-            size="xs"
-            variant="primary"
-          >
-            <SquareCheck className="size-4" />
-            All
-          </Button>
-          <Button onClick={() => onChange([])} outline={selectedOptions.length > 0} size="xs" variant="primary">
-            <Square className="size-4" />
-            None
-          </Button>
-        </Flex>
-      </PopoverContent>
-    </Popover>
+    <Combobox autoHighlight items={flatOptions} multiple>
+      <ComboboxChips className="w-full max-w-xs" ref={anchor}>
+        <ComboboxValue>
+          {(selectedValues) => {
+            const selectedOptions = selectedValues.map((value: string) =>
+              flatOptions.find((option) => option.value === value),
+            );
+            console.log(selectedOptions);
+            return (
+              <>
+                {selectedOptions.map((option: SelectOption) => (
+                  <ComboboxChip key={option.value}>{option.label}</ComboboxChip>
+                ))}
+                <ComboboxChipsInput />
+              </>
+            );
+          }}
+        </ComboboxValue>
+      </ComboboxChips>
+      <ComboboxContent anchor={anchor}>
+        <ComboboxEmpty>No items found.</ComboboxEmpty>
+        <ComboboxList>
+          {(item) => (
+            <ComboboxItem key={item.value} value={item.value}>
+              {item.label}
+            </ComboboxItem>
+          )}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox>
   );
+
+  // return (
+  //   <Popover>
+  //     <PopoverTrigger
+  //       className={cn(
+  //         "multi-select-trigger",
+  //         `multi-select-trigger--${size}`,
+  //         hasFilter && "multi-select-trigger--filtered",
+  //         className,
+  //       )}
+  //     >
+  //       <span className={cn("multi-select-label", hasFilter && "multi-select-label--filtered")}>{displayLabel}</span>
+  //       <ChevronDownIcon className="multi-select-chevron" />
+  //     </PopoverTrigger>
+  //     <PopoverContent className="multi-select-content w-(--anchor-width)">
+  //       <Flex className="multi-select-options" direction="col" gap="4">
+  //         {optionGroups.map((optionGroup, _index) => (
+  //           <Flex direction="col" gap="1" key={optionGroup.label ?? "options"}>
+  //             {optionGroup.label && <p className="multi-select-group-label">{optionGroup.label}</p>}
+  //             {optionGroup.options.map(renderOption)}
+  //           </Flex>
+  //         ))}
+  //       </Flex>
+  //       <Separator />
+  //       <Flex direction="row" gap="2">
+  //         <Button
+  //           onClick={() => onChange(flatOptions.map(({ value }) => value))}
+  //           outline={hasFilter}
+  //           size="xs"
+  //           variant="primary"
+  //         >
+  //           <SquareCheck className="size-4" />
+  //           All
+  //         </Button>
+  //         <Button onClick={() => onChange([])} outline={selectedOptions.length > 0} size="xs" variant="primary">
+  //           <Square className="size-4" />
+  //           None
+  //         </Button>
+  //       </Flex>
+  //     </PopoverContent>
+  //   </Popover>
+  // );
 };
